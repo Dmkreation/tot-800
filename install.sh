ff4af3808452016c3968cf21c287e4c3cb687d1b
David <eth3rnit3@gmail.com>
2023-05-19 16:48:49 +0200

    finish logger

--- install.sh
+++ install.sh
@@ -1,9 +1,13 @@
 #!/bin/zsh
 
 export TOOLS_PATH=/home/tot800/tools
+export INTERNAL_PATH=/home/tot800/internal
 
 # Extractors
 cd $PDF_EXTRACTOR_PATH && pip install -r requirements.txt
 
 # Translators
 cd $FR2EN_TRANSLATOR_PATH && pip install -r requirements.txt
+
+# Internal tools
+cd $INTERNAL_PATH && pip install -r requirements.txt
--- internal/logger.py
+++ internal/logger.py
@@ -1,6 +1,9 @@
 from enum import Enum
 import os
 from datetime import datetime
+from termcolor import colored
+
+print(colored('Ceci est du texte rouge', 'red'))
 
 
 class LogLevel(Enum):
@@ -8,6 +11,14 @@ class LogLevel(Enum):
     WARNING = 3
     ERROR = 2
     DEBUG = 1
+
+
+level_colors = {
+    'INFO': 'white',
+    'WARNING': 'yellow',
+    'ERROR': 'red',
+    'DEBUG': 'white',
+}
 
 
 class Logger:
@@ -39,14 +50,14 @@ class Logger:
         print(self.__message("INFO", message))
 
     def __message(self, level: str, message: str) -> str:
-        return f"{self.__base_message(level)}{message}"
+        return f"{self.__base_message(level)}{colored(message, level_colors[level])}"
 
     def __base_message(self, level: str) -> str:
-        app_name = os.environ.get('APP_NAME', 'TOT-800')
+        app_name = colored(os.environ.get('APP_NAME', 'TOT-800'), 'blue')
         now = datetime.now()
-        fmt_now = now.strftime("%H:%M:%S:%d/%m/%Y")
+        fmt_now = colored(now.strftime("%H:%M:%S:%d/%m/%Y"), 'green')
 
-        return f"[{app_name}-{level}-({fmt_now})] - "
+        return f"[{fmt_now}]-[{app_name}] - {colored(level, level_colors[level])} - "
 
 
 logger = Logger(LogLevel[os.environ.get('LOG_LEVEL', 'DEBUG').upper()])
