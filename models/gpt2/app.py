9e3cb79fe159c70e2bf383cc5160b8e5cb6c6876
David <eth3rnit3@gmail.com>
2023-05-20 16:50:41 +0200

    update gpt2 query

--- models/gpt2/app.py
+++ models/gpt2/app.py
@@ -6,13 +6,11 @@ tokenizer = GPT2Tokenizer.from_pretrained(
 model = GPT2LMHeadModel.from_pretrained(
     f"{os.environ['GPT2_MODEL_PATH']}/model")
 
-input_text = "Le début de votre texte"
-input_ids = tokenizer.encode(input_text, return_tensors='pt')
 
-# Attention: 'max_length' peut être modifié selon le besoin
-sample_outputs = model.generate(
-    input_ids, pad_token_id=50256, max_length=100, do_sample=True, top_k=40)
-
-for i, sample_output in enumerate(sample_outputs):
-    print("Exemple de texte généré: {}".format(
-        tokenizer.decode(sample_output, skip_special_tokens=True)))
+def query_gpt(input: str) -> None:
+    input_text = input
+    input_ids = tokenizer.encode(input_text, return_tensors='pt')
+    sample_outputs = model.generate(
+        input_ids, pad_token_id=50256, max_length=100, do_sample=True, top_k=40)
+    for i, sample_output in enumerate(sample_outputs):
+        print(tokenizer.decode(sample_output, skip_special_tokens=True))
